9e4d330e5f70b46d6e28bec4b5a0c1568c5384b1
jacinta_riko <jaccinadzua@gmail.com>
2018-11-30 17:31:51 +0300

    [starts #162336755]add user get redflags

--- app/api/v1/redflags/models.py
+++ app/api/v1/redflags/models.py
@@ -24,3 +24,5 @@ redflags = DB()
 
     
 
+    
+
--- app/api/v1/redflags/views.py
+++ app/api/v1/redflags/views.py
@@ -1,14 +1,12 @@
 from flask_restful import Resource
-from flask import jsonify, make_response, request, abort     
+from flask import jsonify, make_response, request, abort 
 
 import datetime
 
 incidents = []
 
-
 class RedFlags(Resource):
-    """RedFlags"""
-    
+"""RedFlags"""
     def __init__(self):
         self.db = incidents
         self.id = len(incidents) + 1
@@ -18,7 +16,7 @@ class RedFlags(Resource):
         return make_response(jsonify({
             "status" : 200,
             "data" : self.db
-        }), 200) 
-       
+            }), 200) 
+
     
-    
+
